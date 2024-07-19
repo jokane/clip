8fc0e5c6c508227efb163ce59c18f12ff19231c2
Jason O'Kane <jokane@tamu.edu>
2024-07-19 08:32:25 -0500

    Document from_file.

--- clip/from_file.py
+++ clip/from_file.py
@@ -20,8 +20,8 @@ from .validate import *
 from .util import temporary_current_directory, parse_hms_to_seconds
 
 def get_duration_from_ffprobe_stream(stream):
-    """ Given a dictionary of ffprobe-returned attributes of a stream, try to
-    figure out the duration of that stream. """
+    """Given a dictionary of ffprobe-returned attributes of a stream, try to
+    figure out the duration of that stream."""
 
     if 'duration' in stream and is_float(stream['duration']):
         return float(stream['duration'])
@@ -39,8 +39,8 @@ def get_duration_from_ffprobe_stream(stream):
 
     raise ValueError(f"Could not find a duration in ffprobe stream. {stream}")
 
-def metrics_and_frame_rate_from_stream_dicts(streams, fname):
-    """ Given a dicts representing the audio, video, and subtitles elements of
+def metrics_and_frame_rate_from_stream_dicts(streams, filename):
+    """Given a dict representing the audio, video, and subtitles elements of
     a clip, return the appropriate metrics object, the float framerate, and
     booleans telling whether video, audio, and subtitles exist."""
     video_stream = streams['video']
@@ -63,7 +63,7 @@ def metrics_and_frame_rate_from_stream_dicts(streams, fname):
         vlen = get_duration_from_ffprobe_stream(video_stream)
         alen = get_duration_from_ffprobe_stream(audio_stream)
         if abs(vlen - alen) > 0.5:
-            raise ValueError(f"In {fname}, video length ({vlen}) and audio length ({alen}) "
+            raise ValueError(f"In {filename}, video length ({vlen}) and audio length ({alen}) "
               "do not match. Perhaps load video and audio separately?")
 
         return Metrics(width = eval(video_stream['width']),
@@ -91,24 +91,29 @@ def metrics_and_frame_rate_from_stream_dicts(streams, fname):
                False, True, has_subtitles
     else:
         # Should be impossible to get here, but just in case...
-        raise ValueError(f"File {fname} contains neither audio nor video.") # pragma: no cover
+        raise ValueError(f"File {filename} contains neither audio nor video.") # pragma: no cover
 
-def metrics_from_ffprobe_output(ffprobe_output, fname, suppress=None):
-    """ Given the output of a run of
+def metrics_from_ffprobe_output(ffprobe_output, filename, suppress=None):
+    """Sift through output from `ffprobe` and trying to make a `Metrics` from it.
+
+    :param ffprobe_output: A string containing output from `ffprobe`.
+    :param supress: A list containing some (possibly empty) subset of
+            `"video"`, `"audio"`, and `"subtitle"`.  Streams of those types
+            will be ignored.
+
+    :return: A :class:`Metrics` object based on that data, or raise an
+            exception if something strange is in there.
+
+    The output should specifically be from::
 
         ffprobe -of compact -show_entries stream
 
-    return a Metrics object based on that data, or complain if
-    something strange is in there.
-    print(ffprobe_output)
+    Used indirectly by :class:`from_file`.
+    """
 
-    Suppress should be a list of types to ignore like "video" or "audio" or
-    "subtitle"."""
-
-
-    stream_lists = { 'video': [],
-                       'audio': [],
-                       'subtitle': []}
+    stream_lists = {'video': [],
+                    'audio': [],
+                    'subtitle': []}
 
     if suppress is None:
         suppress = ['data']
@@ -130,7 +135,7 @@ def metrics_from_ffprobe_output(ffprobe_output, fname, suppress=None):
 
         # A special case: Image-based subtitles.  Can't do much with those.
         if t == 'subtitle' and stream['codec_name'] == 'dvd_subtitle' in line:
-            warnings.warn(f'Ignoring image-based subtitles in {fname}.')
+            warnings.warn(f'Ignoring image-based subtitles in {filename}.')
             continue
 
         # Store the stream data based on what type it is.  Ignore streams that
@@ -141,7 +146,7 @@ def metrics_from_ffprobe_output(ffprobe_output, fname, suppress=None):
         if t in stream_lists:
             stream_lists[t].append(stream)
         else:
-            raise ValueError(f"Don't know what to do with {fname}, "
+            raise ValueError(f"Don't know what to do with {filename}, "
               f"which has an unknown stream of type {stream['codec_type']}.")
 
     streams_by_type = {}
@@ -151,40 +156,55 @@ def metrics_from_ffprobe_output(ffprobe_output, fname, suppress=None):
         elif len(streams)==1:
             streams_by_type[t] = streams[0]
         else:
-            warnings.warn(f'In {fname} there are {len(streams)} {t} streams.  Using the first one.')
+            warnings.warn(f'In {filename} there are {len(streams)} {t} streams.  Using the first one.')
             streams_by_type[t] = streams[0]
 
-    return metrics_and_frame_rate_from_stream_dicts(streams_by_type, fname)
+    return metrics_and_frame_rate_from_stream_dicts(streams_by_type, filename)
 
-def audio_samples_from_file(fname, cache, expected_sample_rate, expected_num_channels,
-  expected_num_samples):
-    """ Extract audio data from a file, which may be either a pure audio
-    format or a video file containing an audio stream."""
+def audio_samples_from_file(filename, cache, expected_sample_rate, expected_num_channels,
+                            expected_num_samples):
+    """Extract audio data from a file, which may be either a pure audio format
+    or a video file containing an audio stream.
+    
+    :param filename: The name of the file to read.
+    :param cache: A :class:`ClipCache` that might have the audio we want, or
+            into which it can be stored.
+    :param expected_sample_rate: The sample rate we expect to see in the audio,
+            in samples per second.
+    :param expected_num_channels: The number of channels we expect to see in
+            the audio, usually either `1` or `2`.
+    :param expected_num_samples: The number of audio samples we expect to see
+            in each channel.
+
+    Raise an exception if the audio that turns up does not match the expected
+    sample rate, number of channels, and approximate ---that is, within about 1
+    second--- number of samples.
+    """
 
     # Grab the file's extension.
-    ext = os.path.splitext(fname)[1].lower()
+    ext = os.path.splitext(filename)[1].lower()
 
     # Is it in a format we know how to read directly?  If so, read it and
     # declare victory.
     direct_formats = list(map(lambda x: "." + x.lower(),
       soundfile.available_formats().keys()))
     if ext in direct_formats:
-        print("Reading audio from", fname)
+        print("Reading audio from", filename)
 
         # Acquire the data from the file.
-        data, sample_rate = soundfile.read(fname, always_2d=True)
+        data, sample_rate = soundfile.read(filename, always_2d=True)
 
         # Complain if the sample rates or numbers of channels don't match.
         if sample_rate != expected_sample_rate:
-            raise ValueError(f"From {fname}, expected sample rate {expected_sample_rate},"
+            raise ValueError(f"From {filename}, expected sample rate {expected_sample_rate},"
                 f" but found {sample_rate} instead.")
         if data.shape[1] != expected_num_channels:
-            raise ValueError(f"From {fname}, expected {expected_num_channels} channels,"
+            raise ValueError(f"From {filename}, expected {expected_num_channels} channels,"
                 f" but found {data.shape[1]} instead.")
 
         # Complain if there's a length mismatch longer than about a second.
         if abs(data.shape[0] - expected_num_samples) > expected_sample_rate:
-            raise ValueError(f"From {fname}, got {data.shape[0]}"
+            raise ValueError(f"From {filename}, got {data.shape[0]}"
               f" samples instead of {expected_num_samples}.")
 
         # If there's a small mismatch, just patch it.
@@ -202,17 +222,17 @@ def audio_samples_from_file(fname, cache, expected_sample_rate, expected_num_cha
     # Not a format we can read directly.  Instead, use ffmpeg to get it
     # indirectly.  (...or simply pull it from the cache, if it happens to be
     # there.)
-    cached_filename, success = cache.lookup(fname, 'flac')
+    cached_filename, success = cache.lookup(filename, 'flac')
     if not success:
-        print(f'Extracting audio from {fname}')
+        print(f'Extracting audio from {filename}')
         assert '.flac' in direct_formats
-        full_fname = os.path.join(os.getcwd(), fname)
+        full_filename = os.path.join(os.getcwd(), filename)
         with temporary_current_directory():
-            audio_fname = 'audio.flac'
-            ffmpeg( f'-i {full_fname}',
+            audio_filename = 'audio.flac'
+            ffmpeg( f'-i {full_filename}',
                     '-vn',
-                    f'{audio_fname}')
-            os.rename(audio_fname, cached_filename)
+                    f'{audio_filename}')
+            os.rename(audio_filename, cached_filename)
             cache.insert(cached_filename)
 
     return audio_samples_from_file(cached_filename,
@@ -222,7 +242,15 @@ def audio_samples_from_file(fname, cache, expected_sample_rate, expected_num_cha
                                    expected_num_samples)
 
 def parse_subtitles(srt_text, subtitles_filename=None):
-    """Return a generator for the subtitles parse from the given text."""
+    """Parse a string of SRT subtitles into the form used in this library.
+    
+    :param srt_text: A string containing subtitle text in SRT format.
+    :param subtitles_filename: An optional filename to include if an exception
+            must be raised.
+    :return: A generator that yields subtitles, each a `(start_time, end_time, text)`
+            triple.
+
+    """
     for subtitle in srt_text.split('\n\n'):
         lines = subtitle.split('\n')
         # Ignore lines[0], a sequence number which we don't need.
@@ -235,8 +263,16 @@ def parse_subtitles(srt_text, subtitles_filename=None):
         text = '\n'.join(lines[2:])
         yield (start, end, text)
 
-def subtitles_from_file(fname, cache):
-    """ Extract subtitles from a file."""
+def subtitles_from_file(filename, cache):
+    """ Extract subtitles from a file.
+
+    :param filename: The name of a media file that includes a subtitle stream.
+    :param cache: A :class:`ClipCache` that might have the subtitle stream we
+            want, or into which it can be stored.
+    :return: A generator that yields subtitles, each a `(start_time, end_time, text)`
+            triple.
+
+    """
 
     # What file should the subtitles live in and does it exist already?
     subtitles_filename, exists = cache.lookup('subtitles',
@@ -245,9 +281,9 @@ def subtitles_from_file(fname, cache):
 
     # If we don't already have the subtitles file, use ffmpeg to get it.
     if not exists:
-        print(f'Extracting subtitles from {fname}')
+        print(f'Extracting subtitles from {filename}')
         with temporary_current_directory():
-            ffmpeg( f'-i {fname}',
+            ffmpeg( f'-i {filename}',
                    '-map 0:s:0',
                     f'{subtitles_filename}')
 
@@ -260,18 +296,38 @@ def subtitles_from_file(fname, cache):
 
 class from_file(Clip, FiniteIndexed):
     """A clip read from a file such as an mp4, flac, or other format readable
-    by ffmpeg. |from-source|"""
+    by ffmpeg.
+    
+    :param filename: The source file to import.
+    :param supress: A list containing some (possibly empty) subset of
+            `"video"`, `"audio"`, and `"subtitle"`.  Streams of those types
+            will be ignored.
+    :param cache_dir: The directory to use for the frame cache.
 
-    def __init__(self, fname, suppress=None, cache_dir=None):
+    Details about what is included in the source file are extracted by parsing
+    the output of `ffprobe`.  We make a best effort to sort out the metrics of
+    the available video and audio streams, but encoded video is complicated, so
+    there are surely many variations that are not yet handled correctly.  If
+    you have a video for which this process fails, the maintainers would be
+    interested to see it.
+
+    Video is read by asking `ffmpeg` to "explode" the video stream into
+    individual images.  This process takes some time, so the frames are cached
+    for future runs.  There can potentially be lots of images, so you'll want
+    to keep an eye on available disk space.
+
+    |from-source|"""
+
+    def __init__(self, filename, suppress=None, cache_dir=None):
         Clip.__init__(self)
 
         # Make sure the file exists.
-        if not os.path.isfile(fname):
-            raise FileNotFoundError(f"Could not open file {fname}.")
-        self.fname = os.path.abspath(fname)
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(f"Could not open file {filename}.")
+        self.filename = os.path.abspath(filename)
 
         if cache_dir is None:
-            cache_dir = os.path.join('/tmp/clipcache', re.sub('/', '_', self.fname))
+            cache_dir = os.path.join('/tmp/clipcache', re.sub('/', '_', self.filename))
         else:
             require_string(cache_dir, 'cache directory')
 
@@ -299,13 +355,13 @@ class from_file(Clip, FiniteIndexed):
 
         if exists:
             # Yes. Grab it.
-            print(f"Using cached dimensions for {self.fname}")
+            print(f"Using cached dimensions for {self.filename}")
             with open(dimensions_filename, 'r') as f:
                 deets = f.read()
         else:
             # No.  Get the metrics, then store in the cache for next time.
-            print(f"Probing dimensions for {self.fname}")
-            with subprocess.Popen(f'ffprobe -hide_banner -v error {self.fname} '
+            print(f"Probing dimensions for {self.filename}")
+            with subprocess.Popen(f'ffprobe -hide_banner -v error {self.filename} '
                                   '-of compact -show_entries stream', shell=True,
                                   stdout=subprocess.PIPE) as proc:
                 deets = proc.stdout.read().decode('utf-8')
@@ -316,7 +372,7 @@ class from_file(Clip, FiniteIndexed):
 
         # Parse the (very detailed) ffprobe response to get the metrics we
         # need.
-        response = metrics_from_ffprobe_output(deets, self.fname, suppress)
+        response = metrics_from_ffprobe_output(deets, self.filename, suppress)
 
         self.metrics, self.frame_rate, self.has_video, self.has_audio, self.has_subtitles = response
 
@@ -327,7 +383,7 @@ class from_file(Clip, FiniteIndexed):
 
         if self.has_video:
             index = self.time_to_frame_index(t)
-            return [self.fname, index]
+            return [self.filename, index]
         else:
             return ['solid', {
                 'width': self.metrics.width,
@@ -348,22 +404,21 @@ class from_file(Clip, FiniteIndexed):
             return np.zeros([self.metrics.height, self.metrics.width, 4], np.uint8)
         else:
             index = self.time_to_frame_index(t)
-            fname, exists = self.cache.lookup(f'{index:06d}',
+            filename, exists = self.cache.lookup(f'{index:06d}',
                                               self.cache.frame_format,
                                               use_hash=False)
             if not exists:
                 self.explode()
 
-            return read_image(fname)
+            return read_image(filename)
 
     def get_subtitles(self):
         if self.subtitles is None:
             if self.has_subtitles:
-                self.subtitles = list(subtitles_from_file(self.fname, self.cache))
+                self.subtitles = list(subtitles_from_file(self.filename, self.cache))
             else:
                 self.subtitles = []
         return self.subtitles
-
 
     def explode(self):
         """Expand the requested frames into our cache for later."""
@@ -384,23 +439,23 @@ class from_file(Clip, FiniteIndexed):
             ffmpeg(
                 f'-ss {start_time}',
                 f'-t {length}',
-                f'-i {self.fname}',
+                f'-i {self.filename}',
                 f'-r {self.frame_rate}',
                 '%06d.png',
-                task=f'Exploding {os.path.basename(self.fname)}',
+                task=f'Exploding {os.path.basename(self.filename)}',
                 num_frames=num_frames_expected
             )
 
             # Add each frame that was extracted to the cache.
-            for fname in sorted(glob.glob('*.png')):
-                file_index = int(re.search(r'\d*', fname).group(0)) - 1
+            for filename in sorted(glob.glob('*.png')):
+                file_index = int(re.search(r'\d*', filename).group(0)) - 1
                 shifted_index = start_index + file_index
-                new_fname, exists = self.cache.lookup(f'{shifted_index:06d}',
+                new_filename, exists = self.cache.lookup(f'{shifted_index:06d}',
                                                       self.cache.frame_format,
                                                       use_hash=False)
                 if not exists:
-                    os.rename(fname, new_fname)
-                    self.cache.insert(new_fname)
+                    os.rename(filename, new_filename)
+                    self.cache.insert(new_filename)
 
             # Make sure we got all of the frames we expected to get.
             for index in sorted(self.requested_indices):
@@ -409,20 +464,20 @@ class from_file(Clip, FiniteIndexed):
                 # happen from mis-estimations of the video length, or sometimes
                 # from simply missing frames.  To keep things rolling, let's
                 # fill in a black frame instead.
-                fname, exists = self.cache.lookup(f'{shifted_index:06d}',
+                filename, exists = self.cache.lookup(f'{shifted_index:06d}',
                                                   self.cache.frame_format,
                                                   use_hash=False)
                 if not exists: # pragma: no cover
-                    print(f"[Exploding {self.fname} did not produce frame index={index}. "
+                    print(f"[Exploding {self.filename} did not produce frame index={index}. "
                           "Using black instead.]")
                     fr = np.zeros([self.height(), self.width(), 3], np.uint8)
-                    cv2.imwrite(fname, fr)
-                    self.cache.insert(fname)
+                    cv2.imwrite(filename, fr)
+                    self.cache.insert(filename)
 
     def get_samples(self):
         if self.samples is None:
             if self.has_audio:
-                self.samples = audio_samples_from_file(self.fname,
+                self.samples = audio_samples_from_file(self.filename,
                                                        self.cache,
                                                        self.sample_rate(),
                                                        self.num_channels(),
