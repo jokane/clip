aeed1ee009bfdcf2d28b7cd03998526e6fe7f5ed
Jason O'Kane <jokane@tamu.edu>
2024-07-19 16:14:33 -0500

    Document hold_at_start and hold_at_end.

--- clip/hold.py
+++ clip/hold.py
@@ -7,29 +7,41 @@ from .video import repeat_frame
 from .validate import require_float, require_positive
 
 def hold_at_start(clip, target_length) -> Clip:
-    """Extend a clip by repeating its first frame, to fill a target length.
-    |modify|"""
+    """Extend a clip by to fill a target length by repeating its first frame. |modify|
+
+    :param clip: The original clip.
+    :param target_length: The desired length, in seconds.
+
+    If `target_length` is greater than `clip.length()`, the result will be
+    truncated to `target_length`.
+
+    """
     require_clip(clip, "clip")
     require_float(target_length, "target length")
     require_positive(target_length, "target length")
 
     # Here the repeat_frame almost certainly goes beyond target length, and
     # we force the final product to have the right length directly.  This
-    # prevents getting a blank frame at end in some cases.
+    # prevents getting a blank frame at the end in some cases.
     return chain(repeat_frame(clip, clip.length(), target_length),
                  clip,
                  length=target_length)
 
 def hold_at_end(clip, target_length) -> Clip:
-    """Extend a clip by repeating its last frame, to fill a target length.
-    |modify|"""
+    """Extend a clip by to fill a target length by repeating its final frame. |modify|
+
+    :param clip: The original clip.
+    :param target_length: The desired length, in seconds.
+
+    If `target_length` is greater than `clip.length()`, the result will be
+    truncated to `target_length`.
+
+    """
     require_clip(clip, "clip")
     require_float(target_length, "target length")
     require_positive(target_length, "target length")
 
-    # Here the repeat_frame almost certainly goes beyond target length, and
-    # we force the final product to have the right length directly.  This
-    # prevents getting a blank frame at end in some cases.
+    # Forcing the legnth -- same caveat as above.
     return chain(clip,
                  repeat_frame(clip, clip.length(), target_length),
                  length=target_length)
